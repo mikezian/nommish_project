b14b02a7e338147c7b821822200d4c2cf646331f
michael montesines <michaelmontesines@michaels-MacBook-Pro.local>
2015-09-20 08:19:19 +0400

    added recipemanager class

--- recipe/managers.py
+++ recipe/managers.py
@@ -1,9 +1,13 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.db.models import query
+from django.db import models
+
+from caching.base import CachingQuerySet
 
 
-class RecipeQuerySet(query.QuerySet):
+class RecipeQuerySet(CachingQuerySet, query.QuerySet):
+
     def published_recipes(self):
         return self.filter(large_image__isnull=False)
 
@@ -13,4 +17,15 @@ class RecipeQuerySet(query.QuerySet):
     def recipes_startswith(self, text):
         return self.filter(name__istartswith=text)
 
-RecipeManager = RecipeQuerySet.as_manager
+class RecipeManager(models.Manager):
+    def get_queryset(self):
+        return RecipeQuerySet(self.model, using=self._db)
+
+    def published_recipes(self):
+        return self.get_queryset().published_recipes()
+
+    def latest(self):
+        return self.get_queryset().latest()
+
+    def recipes_startswith(self, text):
+        return self.get_queryset().recipes_startswith()
