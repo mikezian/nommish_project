43d8b4a5c7016381430327115e3c037c10386d49
michael montesines <michaelmontesines@michaels-MacBook-Pro.local>
2015-09-25 14:07:36 +0400

    removed django-secure settings

--- nommish/settings/production.py
+++ nommish/settings/production.py
@@ -9,19 +9,16 @@ ALLOWED_HOSTS = [
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "djangosecure.middleware.SecurityMiddleware",
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-) 
-USE_X_FORWARDED_HOST = True
+)
 
-# django-secure
-SECURE_FRAME_DENY = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
+USE_X_FORWARDED_HOST = True
+X_FRAME_OPTIONS = 'DENY'
 SECURE_BROWSER_XSS_FILTER = True
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_HTTPONLY = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
