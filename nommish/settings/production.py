6b9693070bab106452d12c334f34a8ba1e745865
michael montesines <michaelmontesines@michaels-MacBook-Pro.local>
2015-09-25 13:42:18 +0400

    added django secure settings

--- nommish/settings/production.py
+++ nommish/settings/production.py
@@ -6,3 +6,22 @@ ALLOWED_HOSTS = [
     'nommish.com',  # Allow domain and subdomains
     '.nommish.com',  # Allow domain and subdomains
 ]
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    "djangosecure.middleware.SecurityMiddleware",
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+) 
+USE_X_FORWARDED_HOST = True
+
+# django-secure
+SECURE_FRAME_DENY = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
