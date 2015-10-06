ccf26084639e4b77983bf18a22d446896fc3e582
michael montesines <michaelmontesines@michaels-MacBook-Pro.local>
2015-10-06 23:35:54 +0400

    add test to add and delete recipe from collection

--- recipe/tests/test_models.py
+++ recipe/tests/test_models.py
@@ -7,7 +7,24 @@ from recipe.models import *
 class TestModels(TestCase):
     fixtures = ['recipe/fixtures/test_data.json']
 
+    def setUp(self):
+        self.user = User.objects.create_user('test_user', 'user@test.net', 'secret')
+        self.user.save()
+        self.client = Client()
 
     def test_recipe_count(self):
         recipe = Recipe.objects.all()
         self.assertEquals(len(recipe), 26)
+
+    def test_recipe_to_collection(self):
+        recipe = Recipe.objects.get(pk=29368)
+        current_likes = recipe.likes
+        collection = UserCollection.objects.create_or_addto_collection('my collection', self.user, recipe=recipe)
+        # test if the latest collection cover gets the latest recipe added
+        self.assertEquals(collection.cover, recipe.large_image)
+        # test if the recipe likes was incremented
+        self.assertEquals(recipe.likes, current_likes + 1)
+        collection = UserCollection.objects.remove_from_collection(collection=collection.id, user=self.user, recipe=recipe)
+        # check likes if it has been decremented
+        self.assertEquals(recipe.likes, current_likes)
+        self.assertEquals(len(collection), 0)
